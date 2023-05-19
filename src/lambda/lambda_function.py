cfdc95debdae6bd81069d940b1a217e7b1db6b55
GustavoGomezHiguita <gagomezh@unal.edu.co>
2023-05-19 11:08:31 -0500

    Modifica la función para procesar todas las fuentes de datos del modelo

--- src/lambda/lambda_function.py
+++ src/lambda/lambda_function.py
@@ -1,34 +1,83 @@
 import boto3
 import pandas as pd
 from unidecode import unidecode
-import logging
 
-dict_fields = {
-            'MES': int,
-            'COD_CONFIG_VEHICULO': str,
-            'CONFIG_VEHICULO': str,
-            'CODOPERACIONTRANSPORTE': str,
-            'CODTIPOCONTENEDOR': str,
-            'CODMUNICIPIOORIGEN': int,
-            'CODMUNICIPIODESTINO': int,
-            'CODMERCANCIA': str,
-            'NATURALEZACARGA': str,
-            'VIAJESTOTALES': int,
-            'KILOGRAMOS': float,
-            'GALONES': float,
-            'VIAJESLIQUIDOS': int,
-            'VIAJESVALORCERO': int,
-            'KILOMETROS': float,
-            'VALORESPAGADOS': float,
-            'CODMUNICIPIOINTERMEDIO': int,
-            'KILOMETROSREGRESO': float,
-            'KILOGRAMOSREGRESO': float,
-            'GALONESREGRESO': float
-            }
+file_encoding = 'latin-1'
 
-ls_fields = list(dict_fields.keys())
+def fn_definitions(key):
+    folder = key.split('/')[0]
+    if folder == 'estadisticas':
+        dict_fields = {
+                    'MES': int,
+                    'COD_CONFIG_VEHICULO': str,
+                    'CONFIG_VEHICULO': str,
+                    'CODOPERACIONTRANSPORTE': str,
+                    'CODTIPOCONTENEDOR': str,
+                    'CODMUNICIPIOORIGEN': int,
+                    'CODMUNICIPIODESTINO': int,
+                    'CODMERCANCIA': str,
+                    'NATURALEZACARGA': str,
+                    'VIAJESTOTALES': int,
+                    'KILOGRAMOS': float,
+                    'GALONES': float,
+                    'VIAJESLIQUIDOS': int,
+                    'VIAJESVALORCERO': int,
+                    'KILOMETROS': float,
+                    'VALORESPAGADOS': float,
+                    'CODMUNICIPIOINTERMEDIO': int,
+                    'KILOMETROSREGRESO': float,
+                    'KILOGRAMOSREGRESO': float,
+                    'GALONESREGRESO': float
+                    }
+        extension = '.xlsx'
+        period_col = 'MES'
+        dict_rename = {'MES':'ANOMES'}
 
-def lambda_handler(event):
+
+    elif folder == 'tiempos_logisticos':
+        dict_fields = {
+                    'AÑOMES': int,
+                    'NATURALEZA': str,
+                    'CODIGO_CARGUE': int,
+                    'CODIGO_DESCARGUE': int,
+                    'HORAS_VIAJE': float,
+                    'HORAS_ESPERA_CARGUE': float,
+                    'HORAS_CARGUE': float,
+                    'HORAS_ESPERA_DESCARGUE': float,
+                    'HORAS_DESCARGUE': float,
+                    'CONFIGURACION': str}
+        extension = '.txt'
+        period_col = 'AÑOMES'
+        dict_rename = {
+                    'AÑOMES': 'ANOMES',
+                    'NATURALEZA':'NATURALEZACARGA',
+                    'CONFIGURACION':'COD_CONFIG_VEHICULO',
+                    'CODIGO_CARGUE':'CODMUNICIPIOORIGEN',
+                    'CODIGO_DESCARGUE':'CODMUNICIPIODESTINO'}
+
+    elif folder == 'antiguedad_vehiculos_y_combustible':
+        dict_fields = {
+                    'ANOMES': int,
+                    'CONFIGURACION': str,
+                    'RANGOMODELO': str,
+                    'COMBUSTIBLE': str,
+                    'PLACAS': int,
+                    'VIAJES': int,
+                    'VIAJESVACIOS': int,
+                    'VALORPACTADO': int,
+                    'KILOGRAMOS': int,
+                    'GALONES': int,
+                    'VIAJESVALORCERO': int,
+                    'VIAJESLIQUIDOS': int}
+        extension = '.xlsx'
+        period_col = 'ANOMES'
+        dict_rename = {'CONFIGURACION':'COD_CONFIG_VEHICULO'}
+
+    ls_fields = list(dict_fields.keys())
+
+    return ls_fields, extension, period_col, dict_fields, dict_rename
+
+def lambda_handler(event, context):
     source_bucket = event['Records'][0]['s3']['bucket']['name']
     source_key = event['Records'][0]['s3']['object']['key']
     destination_bucket = 'rndc-refined'
@@ -49,7 +98,7 @@ def lambda_handler(event):
             raise Exception(f"file:{source_key} | error: No se cumple")
 
     except Exception as e:
-        logging.error(f"file:{source_key} | error: {str(e)}")
+        raise Exception(f"file:{source_key} | error: {str(e)}")
 
 
 def fn_transformation(key,file):
@@ -58,21 +107,26 @@ def fn_transformation(key,file):
     
     1. Cargar solo las columnas definidas en el modelo de datos.
     2. Eliminar registros duplicados.
-    3. Convertir a minúsculas los valores tipo str.
+    3. Convertir a mayúsculas los valores tipo str.
     4. Quitar acentos a valores tipo str.
 
     Retorna un dataframe con las transformaciones aplicadas.
     
     """
+    ls_fields, extension, period_col, dict_fields, dict_rename = fn_definitions(key)
 
-    try:
+    if extension == '.txt':
+        df = pd.read_csv(file,usecols=ls_fields,delimiter='|',encoding=file_encoding,dtype=dict_fields)
+    else:
         df = pd.read_excel(file,usecols=ls_fields,dtype=dict_fields)
-        df.drop_duplicates(inplace=True)
-        df = df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
-        df = df.applymap(lambda x: unidecode(x) if isinstance(x, str) else x)
-    except Exception as e:
-        logging.error(f"file:{key} | error: {str(e)}")
+    df.rename(columns=dict_rename,inplace=True)
+    df.drop_duplicates(inplace=True)
+    df = df.applymap(lambda x: x.upper() if isinstance(x, str) else x)
+    df = df.applymap(lambda x: unidecode(x) if isinstance(x, str) else x)
+    if extension == '.txt':
+         df = df.groupby(by=['ANOMES','NATURALEZACARGA','CODMUNICIPIOORIGEN','CODMUNICIPIODESTINO','COD_CONFIG_VEHICULO'],as_index=False).mean()
 
+    return df
 
 
 def fn_validation(key,file):
@@ -86,29 +140,34 @@ def fn_validation(key,file):
     En caso de no cumplir con alguna de las validaciones no se procede con la transformación de los datos y se registra el log.
     """
 
-    try:
+    ls_fields, extension, period_col, dict_fields, dict_rename = fn_definitions(key)
 
-        val = True
+    val = True
 
-        if not key.endswith('.xlsx'):
-            val = False
-            return val
-        
+    # 1.
+    if not key.endswith(extension):
+        val = False
+        return val
+    # 2.
+    if extension == '.txt':
+        df = pd.read_csv(file,nrows=0,delimiter='|',encoding=file_encoding)
+    else:
         df = pd.read_excel(file,nrows=0)
-        val = all(elem in df.columns.to_list() for elem in ls_fields)
+    val = all(elem in df.columns.to_list() for elem in ls_fields)
 
-        if val == False:
-            return val
-
-        period = float(file.split('_')[-1].split('.')[0])
-        df = pd.read_excel(file,usecols=['MES'])
-        period_avg = df.MES.mean()
-        val = period == period_avg
-
-        if val == False:
-            return val
-        
+    if val == False:
         return val
     
-    except Exception as e:
-        logging.error(f"file:{key} | error: {str(e)}")
+    # 3.
+    if extension == '.txt':
+        df = pd.read_csv(file,usecols=[period_col],delimiter='|',encoding=file_encoding)
+    else:
+        df = pd.read_excel(file,usecols=['period_col'])
+    period = float(file.split('_')[-1].split('.')[0])
+    period_avg = df.MES.mean()
+    val = period == period_avg
+
+    if val == False:
+        return val
+    
+    return val
