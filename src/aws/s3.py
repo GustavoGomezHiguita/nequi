5f2071c8c153148f708a4056399ec03b94afedc5
GustavoGomezHiguita <gagomezh@unal.edu.co>
2023-05-19 06:34:01 -0500

    Refactorizar funciones para subir al nuevo bucket 'rndc-raw' solo los archivos que no están en dicho bucket

--- src/aws/s3.py
+++ src/aws/s3.py
@@ -10,19 +10,38 @@ config.read(config_path)
 
 AWS_ACCESS_KEY_ID = config['aws']['aws_access_key_id']
 AWS_SECRET_ACCESS_KEY = config['aws']['aws_secret_access_key']
-BUCKET_NAME = 'prueba-s3-gustavo-gomez'
+BUCKET_NAME = 'rndc-raw'
+base_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 
-client = boto3.client('s3',aws_access_key_id=AWS_ACCESS_KEY_ID,aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
-def upload_file():
-    data_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'data'))
+def fn_upload_files(folder):
+    client = boto3.client('s3',aws_access_key_id=AWS_ACCESS_KEY_ID,aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    data_path = os.path.join(base_directory, 'data', folder)
     file_list = os.listdir(data_path)
+    s3_file_list = fn_get_bucket_file_names(BUCKET_NAME,folder)
     for file_name in file_list:
-        full_path = f"{data_path}\{file_name}"
-        with open(full_path,'rb') as file:
-            object_key = f"data/{file_name}"
-            client.upload_fileobj(file, BUCKET_NAME, object_key)
-            print('file_uploaded')
+        if file_name not in s3_file_list:
+            full_path = f"{data_path}\{file_name}"
+            with open(full_path,'rb') as file:
+                object_key = f"{folder}/{file_name}"
+                client.upload_fileobj(file, BUCKET_NAME, object_key)
+                print(f'folder: {folder} | file: {file_name} | uploaded')
+
+def fn_get_bucket_file_names(bucket_name,folder):
+    session = boto3.Session(aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    s3 = session.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    file_names = []
+    for obj in bucket.objects.filter(Prefix=folder):
+        file_name = obj.key.split('/')[-1]
+        file_names.append(file_name)
+    return file_names
+
+def fn_upload_folder_files():
+
+    fn_upload_files('estadisticas')
+    fn_upload_files('tiempos_logisticos')
+    fn_upload_files('antiguedad_vehiculos_y_combustible')
 
 if __name__=='__main__':
-    upload_file()
+    fn_upload_folder_files()
